24423d2e7cbfea7a982d1f4ce9a1377c971b28c1
Ryan Jokhu <jokhu6833@gmail.com>
2025-03-06 00:34:33 -0500

    1.0

---
+++ Configuration.cs
@@ -0,0 +1,29 @@
+﻿using Rocket.API;
+
+namespace Kill4Heal
+{
+    public class Configuration : IRocketPluginConfiguration
+    {
+        public uint Skull_HEALTH, Arm_HEALTH, Leg_HEALTH, Body_HEALTH, Foot_HEALTH, Spine_HEALTH, Front_HEALTH, Back_HEALTH;
+        public bool ConsoleMsgEnabled, PlayerMsgEnabled;
+        public float plrMsg_r, plrMsg_g, plrMsg_b;
+        public string consoleMsgColor;
+        public void LoadDefaults()
+        {
+            Skull_HEALTH = 75;
+            Arm_HEALTH = 30;
+            Leg_HEALTH = 20;
+            Body_HEALTH = 50;
+            Foot_HEALTH = 10;
+            Spine_HEALTH = 40;
+            Front_HEALTH = 20;
+            Back_HEALTH = 20;
+            ConsoleMsgEnabled = true;
+            PlayerMsgEnabled = true;
+            plrMsg_r = 255;
+            plrMsg_g = 0;
+            plrMsg_b = 0;
+            consoleMsgColor = "red";
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using Rocket.Unturned.Events;
+using SDG.Unturned;
+using Rocket.API.Collections;
+using UnityEngine;
+
+namespace Kill4Heal
+{
+    public class Kill4Heal : RocketPlugin<Configuration>
+    {
+        public static Kill4Heal Instance;
+
+        protected override void Load()
+        {
+            Rocket.Core.Logging.Logger.Log("=================");
+            Rocket.Core.Logging.Logger.Log("Kill4Heal Loaded!");
+            Rocket.Core.Logging.Logger.Log("created by: Ryan");
+            Rocket.Core.Logging.Logger.Log("=================");
+            UnturnedPlayerEvents.OnPlayerDeath += Events_OnPlayerDeath;
+        }
+        protected override void Unload()
+        {
+            UnturnedPlayerEvents.OnPlayerDeath -= Events_OnPlayerDeath;
+            Rocket.Core.Logging.Logger.Log("Kill4Heal Unloaded!");
+        }
+
+        public void Events_OnPlayerDeath(UnturnedPlayer player, EDeathCause cause, ELimb limb, Steamworks.CSteamID murderer)
+        {
+            UnturnedPlayer killer = UnturnedPlayer.FromCSteamID(murderer);
+            switch (limb)
+            {
+                case ELimb.SKULL:
+                    GiveHealth(Instance.Configuration.Instance.Skull_HEALTH, killer, player, Translate("skull_name"));
+                    break;
+                case ELimb.LEFT_ARM:
+                    GiveHealth(Instance.Configuration.Instance.Arm_HEALTH, killer, player, Translate("arm_name"));
+                    break;
+                case ELimb.RIGHT_ARM:
+                    GiveHealth(Instance.Configuration.Instance.Arm_HEALTH, killer, player, Translate("arm_name"));
+                    break;
+                 case ELimb.LEFT_FOOT:
+                    GiveHealth(Instance.Configuration.Instance.Foot_HEALTH, killer, player, Translate("foot_name"));
+                    break;
+                case ELimb.RIGHT_FOOT:
+                    GiveHealth(Instance.Configuration.Instance.Foot_HEALTH, killer, player, Translate("foot_name"));
+                    break;
+                case ELimb.RIGHT_FRONT:
+                    GiveHealth(Instance.Configuration.Instance.Front_HEALTH, killer, player, Translate("front_name"));
+                    break;
+                case ELimb.LEFT_FRONT:
+                    GiveHealth(Instance.Configuration.Instance.Front_HEALTH, killer, player, Translate("front_name"));
+                    break;
+                case ELimb.SPINE:
+                    GiveHealth(Instance.Configuration.Instance.Spine_HEALTH, killer, player, Translate("spine_name"));
+                    break;
+                case ELimb.LEFT_LEG:
+                    GiveHealth(Instance.Configuration.Instance.Leg_HEALTH, killer, player, Translate("leg_name"));
+                    break;
+                case ELimb.RIGHT_LEG:
+                    GiveHealth(Instance.Configuration.Instance.Leg_HEALTH, killer, player, Translate("leg_name"));
+                    break;
+                case ELimb.LEFT_BACK:
+                    GiveHealth(Instance.Configuration.Instance.Back_HEALTH, killer, player, Translate("back_name"));
+                    break;
+                case ELimb.RIGHT_BACK:
+                    GiveHealth(Instance.Configuration.Instance.Back_HEALTH, killer, player, Translate("back_name"));
+                    break;
+
+        }
+    }
+
+        public void GiveHealth(uint amount, UnturnedPlayer player, UnturnedPlayer killed, string limbName)
+        {
+            if (player.Health < 100) 
+            {
+                byte healAmount = (byte)Math.Min(amount, 100 - player.Health);
+                player.Heal((byte)healAmount);
+            }
+            Color c = new Color();
+            c.r = Instance.Configuration.Instance.plrMsg_r;
+            c.g = Instance.Configuration.Instance.plrMsg_g;
+            c.b = Instance.Configuration.Instance.plrMsg_b;
+            UnturnedChat.Say(player, Translate("msg_playermsg", amount, killed, limbName), c);
+            ConsoleColor c2 = ConsoleColor.White;
+            try
+            {
+                c2 = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), Instance.Configuration.Instance.consoleMsgColor, true);
+                Rocket.Core.Logging.Logger.Log(Translate("msg_consolemsg"), c2);
+            }
+            catch
+            {
+                Rocket.Core.Logging.Logger.Log("[Kill4Heal] Please check your color syntax!");
+                Rocket.Core.Logging.Logger.Log(Translate("msg_consolemsg"), ConsoleColor.Red);
+            }
+        }
+
+        public new TranslationList DefaultTranslations
+        {
+            get
+            {
+                return new TranslationList(){
+                    {"msg_playermsg","You received {0} health for killing {1} in the {2}."},
+                    {"msg_consolemsg","{0} received {1} health for killing {2} in the {3}."},
+                    {"spine_name", "spine"},
+                    {"arm_name", "arm"},
+                    {"leg_name", "leg"},
+                    {"back_name", "back"},
+                    {"front_name", "front"},
+                    {"foot_name", "foot"},
+                    {"skull_name", "skull"}
+                };
+            }
+        }
+    }
+}
